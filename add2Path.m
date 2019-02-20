383c194290aba327afd2fc29425a8ab9466f4271
savvast <47782147+savvast@users.noreply.github.com>
2019-02-20 14:02:10 +0000

    no message

--- add2Path.m
+++ add2Path.m
@@ -1,13 +1,7 @@
-dir{1}=[pwd '/src/Analysis'];
-dir{2}=[pwd '/src/ElmLb'];
-dir{3}=[pwd '/src/IO'];
-dir{4}=[pwd '/src/MPM'];
-dir{5}=[pwd '/src/MtlLb'];
-dir{6}=[pwd '/src/post'];
-dir{7}=[pwd '/src/Utilities'];
+dir{1}=[pwd '/src'];
 
 numPaths=length(dir);
 
 for ipath=1:numPaths
-    addpath(dir{ipath})
+    addpath(genpath(dir{ipath}))
 end
